c24a7302a39fbe6de12582a1bf1fdfc18623268e
Fabian Vilers <fabian.vilers@dev-one.com>
2016-05-09 18:14:48 +0200

    Created the Decorator project

---
+++ DesignPatterns/DesignPatterns.Decorator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatterns.Decorator
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
