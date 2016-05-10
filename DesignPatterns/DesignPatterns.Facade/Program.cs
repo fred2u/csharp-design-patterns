5a807d7f8d93cff4fa735a50d28ea341e0ae3fb1
Fabian Vilers <fabian.vilers@dev-one.com>
2016-05-10 19:35:35 +0200

    Added the Facade project

---
+++ DesignPatterns/DesignPatterns.Facade/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.Facade
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
