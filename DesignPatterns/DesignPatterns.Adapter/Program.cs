e765d5182bc0577302ab2620ad3f2ec7852a0a90
Fabian Vilers <fabian.vilers@dev-one.com>
2016-05-10 18:45:43 +0200

    Added the Adapter project

---
+++ DesignPatterns/DesignPatterns.Adapter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.Adapter
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
