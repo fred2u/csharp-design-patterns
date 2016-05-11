7586853946f99501abfea4e9ad7b970dd1778edc
Fabian Vilers <fabian.vilers@dev-one.com>
2016-05-11 13:16:47 +0200

    Added the Singleton project

---
+++ DesignPatterns/DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
