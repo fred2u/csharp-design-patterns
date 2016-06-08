7af760b59d681653e6a9fe09b39361afb58b764d
Fabian Vilers <fabian.vilers@dev-one.com>
2016-06-08 13:57:59 +0200

    Added the Strategy project

---
+++ DesignPatterns/DesignPatterns.Strategy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.Strategy
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
