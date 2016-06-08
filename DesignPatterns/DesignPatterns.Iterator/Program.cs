70dd7771ab109d0b84b2dc17555ca07f51d1039d
Fabian Vilers <fabian.vilers@dev-one.com>
2016-06-08 14:04:19 +0200

    Added the Iterator project

---
+++ DesignPatterns/DesignPatterns.Iterator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.Iterator
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
