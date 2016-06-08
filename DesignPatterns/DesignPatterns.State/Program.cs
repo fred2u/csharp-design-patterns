c3f1f0de9fcc75cbd1d8673a38ebc6be542c8cae
Fabian Vilers <fabian.vilers@dev-one.com>
2016-06-08 13:58:51 +0200

    Added the State project

---
+++ DesignPatterns/DesignPatterns.State/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.State
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
