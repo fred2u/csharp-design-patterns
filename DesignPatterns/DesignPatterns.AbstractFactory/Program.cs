e962aa4e27fbd4acba3f83438e276cc960165708
Fabian Vilers <fabian.vilers@dev-one.com>
2016-06-08 13:56:17 +0200

    Added the AbstractFactory project

---
+++ DesignPatterns/DesignPatterns.AbstractFactory/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.AbstractFactory
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
