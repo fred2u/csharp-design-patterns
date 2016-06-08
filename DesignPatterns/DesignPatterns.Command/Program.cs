f21745b3a7ac0e6e3faf0ec07c4fe739aa70227d
Fabian Vilers <fabian.vilers@dev-one.com>
2016-06-08 14:03:36 +0200

    Added the Command project

---
+++ DesignPatterns/DesignPatterns.Command/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.Command
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
