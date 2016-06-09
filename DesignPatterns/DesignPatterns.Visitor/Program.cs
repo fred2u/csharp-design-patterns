5ee222d4306b08b0cdf277ed7c1cb2154f330424
Fabian Vilers <fabian.vilers@dev-one.com>
2016-06-09 20:45:59 +0200

    Added the Visitor project

---
+++ DesignPatterns/DesignPatterns.Visitor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.Visitor
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
