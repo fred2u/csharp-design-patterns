d8ef3f5f89b000c5a0e0c9a96759897438fd40dd
Fabian Vilers <fabian.vilers@dev-one.com>
2016-06-09 19:52:51 +0200

    Added the Iterator code

--- DesignPatterns/DesignPatterns.Iterator/Program.cs
+++ DesignPatterns/DesignPatterns.Iterator/Program.cs
@@ -6,6 +6,13 @@ namespace DesignPatterns.Iterator
     {
         private static void Main()
         {
+            var values = new [] { 1, 2, 3, 4, 5};
+
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
+
             Console.WriteLine();
             Console.WriteLine("Press any key...");
             Console.Read();
