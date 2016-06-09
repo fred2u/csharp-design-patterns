a7adc42361afa9d63455bb0d3a63a68841edca07
Fabian Vilers <fabian.vilers@dev-one.com>
2016-06-09 21:11:15 +0200

    Added the Memento project

---
+++ DesignPatterns/DesignPatterns.Memento/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.Memento
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
